86a3c214f8671f4945a77bc6b38d0d3d0b029a72
Miray Buyukkaray <miraybuyukkaray@gmail.com>
2023-05-04 13:09:25 +0200

    uploaded the project files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using InvoicePrinter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoicePrinter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Invoices()
+        {
+            Invoice[] invoices = Invoice.invoices;
+
+            return View(invoices);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Invoice.cs
@@ -0,0 +1,30 @@
+﻿namespace InvoicePrinter.Models
+{
+    public class Invoice
+    {
+        public string? Item { get; set; }
+        public double? Price { get; set; }
+
+        public Invoice(string? item, double price)
+        {
+            Item = item;
+            Price = price;
+        }
+
+        public static Invoice[] invoices =
+        {
+            new Invoice("IPhone11", 900.70),
+            new Invoice ("Tv", 2000.50),
+            new Invoice ("Ipad", 840.35),
+			new Invoice ("Printer", 840.35),
+			new Invoice ("Vacuum Cleaner", 75.34),
+			new Invoice ("Smart Watch", 840.35),
+			new Invoice ("Headphones", 112.00),
+			new Invoice ("Mouse", 52.30),
+			new Invoice ("Keyboard", 182.00),
+			new Invoice ("Coffee Machine", 225.35),
+			
+
+		};
+    }
+}
